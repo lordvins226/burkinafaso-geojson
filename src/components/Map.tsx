88941ed40cc9d754c6eaa64a44b89f9eb0e18be8
Kevin Wilfried <kevineilboudo54@gmail.com>
2025-08-16 15:53:57 +0000

    fix: replace osm tiles with no-labels version to remove background city names

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -196,13 +196,13 @@ const Map = ({ selectedLayer, onFeatureClick, selectedFeature }: MapProps) => {
         {boundaryData ? (
           <BoundaryTileLayer
             boundaryData={boundaryData}
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
           />
         ) : (
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
           />
         )}
         {geoData && (
